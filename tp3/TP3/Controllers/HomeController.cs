0079c3fe4e9ff4cbbb11f2b46abbe2c82d414ea7
zak-soussi <92606256+zak-soussi@users.noreply.github.com>
2023-01-08 16:24:37 +0100

    final commit

---
+++ tp3/TP3/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
+using System.Diagnostics;
+using TP3.Models;
+//using System.Data.SQLite;
+namespace TP3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            SQLiteConnection dbCon = new SQLiteConnection("Data Source=C:\\tp.net\\tp3- SQLite database.db;");
+            dbCon.Open();
+
+            using (dbCon)
+            {
+                SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM personal_info", dbCon);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        int id = (int)reader["id"];
+                        string first_name = (string)reader["first_name"];
+                        string last_name = (string)reader["last_name"];
+                        string email = (string)reader["email"];
+                        string image = (string)reader["image"];
+                        string country = (string)reader["country"];
+
+                        Debug.WriteLine("id = {0} first name = {1}  last name = {2}  email={3}  image= {4}  country= {5}  date=", id, first_name, last_name, email, image, country);
+                    }
+
+
+                }
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
